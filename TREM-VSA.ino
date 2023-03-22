9365fc67e56f5615ca48f4f7925777dbf563bb9d
whes1015 <44525760+whes1015@users.noreply.github.com>
2023-03-23 04:47:25 +0800

    Add files via upload

--- TREM-VSA.ino
+++ TREM-VSA.ino
@@ -6,53 +6,55 @@
    Discord 群組 => https://exptech.com.tw/f?v=discord
  **/
 
-// >>> version 1.1.1 <<<
+// >>> version 2.0.0 <<<
 
-#include <Servo.h> //伺服馬達 庫
-#include <ArduinoJson.h> // Json 庫
-#include <HTTPClient.h> // HTTP 庫
+#include <ESP32Servo.h>   //伺服馬達 庫
+#include <ArduinoJson.h>  // Json 庫
+#include <HTTPClient.h>   // HTTP 庫
 
-StaticJsonDocument<4096> JSON; // 初始化 Json
-Servo myservo; // 初始化 伺服馬達
+StaticJsonDocument<4096> JSON;  // 初始化 Json
+Servo myservo;                  // 初始化 伺服馬達
 
-const char* ssid = "ASUS_A8"; // Wi-Fi SSID
-const char* password = "koala_3249"; // Wi-Fi Password
-String station = "H-979-11336952-11"; // 欲使用之測站 UUID
-int enlarge = 10; // H 版取 10 L 取 1
+const char* ssid = "ASUS_A8";                     // Wi-Fi SSID
+const char* password = "koala_3249";              // Wi-Fi Password
+String station = "H-979-11336952-11";             // 欲使用之測站 UUID
+String key = "9b2db182d6a7603c7152d46d8d633dba";  // 金鑰
+int enlarge = 1000;                                 // H 版取 1000 L 取 1
 
 float angle(float a) {
-  if (a > 25) return 25;
-  if (a < -25) return -25;
+  if (a > 115) return 115;
+  if (a < 65) return 65;
   return a;
 }
 
 void setup() {
-  myservo.attach(13); // 定義 伺服馬達 腳位
-  myservo.write(90); // 初始化(置中) 伺服馬達 角度
-  pinMode(LED_BUILTIN, OUTPUT); // 設置 LED 腳位為 OUTPUT
-  digitalWrite(LED_BUILTIN, HIGH); // 開啟 LED
-  WiFi.begin(ssid, password); // 連接至 Wi-Fi
-  for (int i = 0; WiFi.status() != WL_CONNECTED; i++) { // Wi-Fi 連接
+  myservo.setPeriodHertz(50);
+  myservo.attach(13, 500, 2400);                         // 定義 伺服馬達 腳位
+  myservo.write(90);                                     // 初始化(置中) 伺服馬達 角度
+  pinMode(LED_BUILTIN, OUTPUT);                          // 設置 LED 腳位為 OUTPUT
+  digitalWrite(LED_BUILTIN, HIGH);                       // 開啟 LED
+  WiFi.begin(ssid, password);                            // 連接至 Wi-Fi
+  for (int i = 0; WiFi.status() != WL_CONNECTED; i++) {  // Wi-Fi 連接
     delay(200);
     if (i > 30) {
-      ESP.restart(); // 連接失敗重啟
+      ESP.restart();  // 連接失敗重啟
     }
   }
-  digitalWrite(LED_BUILTIN, LOW); // 關閉 LED
+  digitalWrite(LED_BUILTIN, LOW);  // 關閉 LED
 }
 
 void loop() {
-  HTTPClient http; // HTTP 對象
-  http.begin("http://exptech.com.tw/api/v1/trem/original?uuid=" + station + "&type=original-vector"); // 初始化 HTTP GET 請求
-  int httpResponseCode = http.GET(); // 發起 HTTP GET 請求
+  HTTPClient http;                                                                                                // HTTP 對象
+  http.begin("http://exptech.com.tw/api/v1/trem/original?uuid=" + station + "&type=original-vector&key=" + key);  // 初始化 HTTP GET 請求
+  int httpResponseCode = http.GET();                                                                              // 發起 HTTP GET 請求
   if (httpResponseCode == 200) {
     JSON.clear();
-    deserializeJson(JSON, http.getString()); // Json 反序列化
-    for (int i = 0; i < JSON["length"].as<float>(); i++) { // 控制 伺服馬達
-      myservo.write(angle(90 + JSON["Z"][i].as<float>()*enlarge)); // >>> 取的軸向 ( X、Y、Z、PGA ) <<<
-      delay(50); // 原始數據為 20Hz 故此處 delay 50ms
+    deserializeJson(JSON, http.getString());                          // Json 反序列化
+    for (int i = 0; i < JSON["length"].as<float>(); i++) {            // 控制 伺服馬達
+      myservo.write(angle(90 + JSON["Z"][i].as<float>() * enlarge));  // >>> 取的軸向 ( X、Y、Z、PGA ) <<<
+      delay(50);                                                      // 原始數據為 20Hz 故此處 delay 50ms
     }
   }
-  if (httpResponseCode == -1) ESP.restart(); // 例外情況
-  http.end(); // 結束 HTTP
+  if (httpResponseCode == -1) ESP.restart();  // 例外情況
+  http.end();                                 // 結束 HTTP
 }
